c252b6a9588245e1b83ba4369113eacbdec53b91
Abdimalik-Osman <engabdimalik8818@gmail.com>
2024-03-23 10:01:09 +0300

    user validation added

---
+++ src/users/validation.js
@@ -0,0 +1,45 @@
+import { body } from "express-validator";
+import mongoose from "mongoose";
+
+export const validate = [
+
+  // username validation
+  body("username").notEmpty()
+  .withMessage("Username is required")
+  .isLength({ min: 3 })
+  .withMessage("Username must be at least 3 characters"),
+
+  // password validation
+  body("password").notEmpty()
+  .withMessage("password is required")
+  .isLength({ min: 3 })
+  .withMessage("password must be at least 3 characters"),
+
+    // full name validation
+  body("fullName").notEmpty().withMessage("full name is required")
+  .isLength({ min: 3 })
+  .withMessage("Full Name must be at least 3 characters"),,
+
+  // user role validation
+  body("role")
+    .notEmpty()
+    .withMessage("user role is required"),
+
+  // Department ID  validation
+  body("departmentId")
+    .notEmpty()
+    .withMessage("Department ID is required"),
+
+    // user status validation
+  body("status")
+    .notEmpty()
+    .withMessage("User Status is required"),
+
+    // created user validation
+  body("createdBy")
+    .notEmpty()
+    .withMessage("Created User or createdBy is required"),
+
+  // Description validation
+  body("description").optional(),
+];
