5d9dfde4ec19c2fb48f47eb05b5eea2ce18f9f59
Hafsa Abdukadir <hafzaabdulkhadir@gmail.com>
2024-08-26 00:08:51 +0300

    Reports

--- src/reports/route.js
+++ src/reports/route.js
@@ -11,9 +11,24 @@ import {
   TotalCustomers,
   getLastFiveInvoices,
   getEmployeeSalesReport,
+  getPurchaseReport,
+  TotalLoan,
 
 } from "./controller.js";
 
 const router = express.Router();
 
-router.get("/receivables", TotalReceivables);
+router.get("/receivables", AuthMiddleware, TotalReceivables);
+router.get("/received", AuthMiddleware, TotalAmountReceived);
+router.get("/products", AuthMiddleware, TotalProducts);
+router.get("/profit", AuthMiddleware, TotalProfit);
+router.get("/expenses", AuthMiddleware, totalExpenses);
+router.get("/customers", AuthMiddleware, TotalCustomers);
+router.get("/employees", AuthMiddleware, TotalEmployees);
+router.get("/users", AuthMiddleware, TotalUsers);
+router.get("/suppliers", AuthMiddleware, TotalSuppliers);
+router.get("/invoices", AuthMiddleware, getLastFiveInvoices);
+router.get("/employee-sales", AuthMiddleware, getEmployeeSalesReport);
+router.get("/purchase-report", AuthMiddleware, getPurchaseReport);
+router.get("/loans-report", AuthMiddleware, TotalLoan);
+export default router;
